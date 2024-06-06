2d1dceecd4ce9896d81d7b5ee9341a43903b6a38
Diogo Fernandes <diogoafernandes20@gmail.com>
2024-06-06 11:48:54 +0100

    Componentes login e registo

---
+++ Website/src/app/regist/regist.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistComponent } from './regist.component';
+
+describe('RegistComponent', () => {
+  let component: RegistComponent;
+  let fixture: ComponentFixture<RegistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RegistComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(RegistComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Website/src/app/regist/regist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regist',
+  standalone: true,
+  imports: [],
+  templateUrl: './regist.component.html',
+  styleUrl: './regist.component.css'
+})
+export class RegistComponent {
+
+}
