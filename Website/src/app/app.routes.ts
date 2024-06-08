bdf52400fb76535e06506d0b9808102c46ea5107
pedro28rei <a26013@alunos.ipca.pt>
2024-06-08 13:50:01 +0100

    Login page UPDATE

--- Website/src/app/app.routes.ts
+++ Website/src/app/app.routes.ts
@@ -1,9 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { SupportComponent } from './support/support.component';
 import { MuseumComponent } from './museum/museum.component';
+import { LoginComponent } from './login/login.component';
+import { RegistComponent } from './regist/regist.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
@@ -11,5 +14,7 @@ export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'contacts', component: ContactsComponent },
     { path: 'support', component: SupportComponent },
-    { path: 'museum/:id', component: MuseumComponent }
+    { path: 'museum/:id', component: MuseumComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'login', component: RegistComponent },
 ];
--- Website/src/app/login/login.component.ts
+++ Website/src/app/login/login.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  loginUser() {
+    // Your login logic here
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
